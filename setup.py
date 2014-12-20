71e4b6dce7b041190044853f6544abb3c8994223
Max Zheng <mzheng@linkedin.com>
2014-12-20 05:59:18 -0800

    Initial setup

---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python2.6
+
+import os
+import setuptools
+
+
+setuptools.setup(
+  name='clicast',
+  version='0.0.1',
+
+  author='Max Zheng',
+  author_email='mzheng@linkedin.com',
+
+  description=open('README.rst').read(),
+
+  entry_points={
+    'console_scripts': [
+      'cast-example = clicast.controller:main',
+    ],
+  },
+
+  install_requires=[],
+
+  license='MIT',
+
+  package_dir={'': 'src'},
+  packages=setuptools.find_packages('src'),
+  include_package_data=True,
+
+  setup_requires=['setuptools-git'],
+
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Development Tools',
+
+    'License :: OSI Approved :: MIT License',
+
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+  ],
+
+  keywords='cli broadcast command warning critical bug',
+)
---
+++ test/test_scripts.py
@@ -0,0 +1,13 @@
+import os
+import pytest
+import subprocess
+
+
+@pytest.mark.parametrize("script", ['cast-example'])
+def test_script_sanity(script):
+
+  try:
+    subprocess.check_output([script, '-h'], stderr=subprocess.STDOUT)
+  except subprocess.CalledProcessError as e:
+    print e.output
+    assert e.returncode == 0
