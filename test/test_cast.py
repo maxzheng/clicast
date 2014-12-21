60d905514492e12b6844bd0ac47a1c0d1bfa2500
Max Zheng <mzheng@linkedin.com>
2014-12-21 14:47:41 -0800

    Fix example and test

--- test/test_cast.py
+++ test/test_cast.py
@@ -5,25 +5,25 @@ import pytest
 from clicast.cast import Cast, CastReader
 
 
-def casts():
-  cast_file = os.path.join(os.path.dirname(__file__), 'example.cast')
-  return [Cast.from_file(cast_file),
-          Cast.from_url('https://raw.githubusercontent.com/maxzheng/clicast/master/test/example.cast')]
+cast_file = os.path.join(os.path.dirname(__file__), 'example.cast')
+cast = Cast.from_file(cast_file)
 
 
 class TestCast(object):
-  @pytest.mark.parametrize('cast', casts())
-  def test_from(self, cast):
+  def test_from(self):
     assert cast.alert == 'We found a big bad bug. Please try not to step on it!! Icky...\nNo worries. It will be fixed soon! :)'
     assert cast.alert_exit
     assert [m.message for m in cast.messages] == [
-      'Version 0.1 has been released! Upgrade today to get cool features.',
-      'Version 0.2 has been released! If you upgrade, you will get:\n'
+      'Version 0.2 has been released! Upgrade today to get cool features.',
+      'Version 0.1 has been released! If you upgrade, you will get:\n'
       '1) Cool feature 1\n'
       '2) Cool feature 2\n'
       'So what are you waiting for? :)',
       'There is a small bug over there, so watch out!']
 
+  def test_from_url(self):
+    cast = Cast.from_url('https://raw.githubusercontent.com/maxzheng/clicast/master/test/example.cast')
+    assert cast.messages
 
 class TestCastReader(object):
   def setup_class(cls):
@@ -32,11 +32,11 @@ class TestCastReader(object):
       os.unlink(CastReader.READ_MSG_FILE)
 
   def test_new_messages(self):
-    reader = CastReader(casts()[0])
+    reader = CastReader(cast)
     assert reader.new_messages() == [
       'We found a big bad bug. Please try not to step on it!! Icky...\nNo worries. It will be fixed soon! :)',
-      'Version 0.1 has been released! Upgrade today to get cool features.',
-      'Version 0.2 has been released! If you upgrade, you will get:\n'
+      'Version 0.2 has been released! Upgrade today to get cool features.',
+      'Version 0.1 has been released! If you upgrade, you will get:\n'
       '1) Cool feature 1\n'
       '2) Cool feature 2\n'
       'So what are you waiting for? :)',
