daffc327b784b43f9dc56ee6a7ea36888f332423
Max Zheng <mzheng@linkedin.com>
2015-01-06 00:45:54 -0800

    Actually set cache_duration on RemoteConfig and remove unused url_content()

--- src/clicast/cast.py
+++ src/clicast/cast.py
@@ -139,7 +139,7 @@ class Cast(object):
                                avoid downloading too often.
     :rtype: :class:`Cast`
     """
-    config = RemoteConfig(context, compact_form=True)
+    config = RemoteConfig(context, cache_duration=cache_duration, compact_form=True)
 
     alert_msg = None
     alert_exit = None
@@ -298,46 +298,5 @@ def _re_sub_multiline(pattern, repl, string):
     for line in string.split('\n'):
       content.append(re.sub(pattern, repl, line))
     content = '\n'.join(content)
-
-  return content
-
-
-def _url_content_cache_file(url):
-  return os.path.join(tempfile.gettempdir(), 'url-content-cache-%s' % base64.urlsafe_b64encode(url))
-
-
-def url_content(url, cache_duration=None, from_cache_on_error=False):
-  """
-  Get content for the given URL
-
-  :param str url: The URL to get content from
-  :param int cache_duration: Optionally cache the content for the given duration to avoid downloading too often.
-  :param bool from_cache_on_error: Return cached content on any HTTP request error if available.
-  """
-  cache_file = _url_content_cache_file(url)
-
-  if cache_duration:
-    if os.path.exists(cache_file):
-      stat = os.stat(cache_file)
-      cached_time = stat.st_mtime
-      if time.time() - cached_time < cache_duration:
-        with open(cache_file) as fp:
-          return fp.read()
-
-  try:
-    response = requests.get(url)
-    response.raise_for_status()
-    content = response.text
-
-  except Exception:
-    if from_cache_on_error and os.path.exists(cache_file):
-      with open(cache_file) as fp:
-        return fp.read()
-    else:
-      raise
-
-  if cache_duration or from_cache_on_error:
-    with open(cache_file, 'w') as fp:
-      fp.write(content)
 
   return content
